106f90bec4db4ce04b43bd4308b1a7782b5ad847
Egnat <sergey_p_i@mail.ru>
2022-12-06 19:43:13 +0300

    (all)

--- script.js/main.js
+++ script.js/main.js
@@ -37,3 +37,47 @@ for (i = 0; i < acc.length; i++) {
     this.classList.toggle("active");
   });
 }
+
+//Смена цвета у слов // массив цветов
+//Функции и переменные не должно называться одинаково для использования двух или ольше элементов
+let colorNames = ["blue", "cyan", "red", "gold", "#d51e8f", "yellow", "orange"];
+
+// функция поиска random-числа
+const random = (min, max) => {
+  max = colorNames.length - 1;
+  const rand = min + Math.round(Math.random() * (max - min));
+  return rand;
+};
+
+// найти элементы для изменения свойств
+let paragraphs = document.getElementsByClassName("contentTextBigPhotoTema");
+
+// менять цвет каждую секунду, присвоив каждому эл-ту массива paragraphs новое значение
+setInterval(() => {
+  for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = colorNames[random(0)];
+  }
+}, 2000);
+console.log(colorNames);
+
+//Смена цвета у слов // массив цветов
+//Функции и переменные не должно называться одинаково для использования двух или ольше элементов
+let colorNamess = ["blue", "cyan", "red", "gold", "#50ff00f5", "yellow", "orange"];
+
+// функция поиска random-числа
+const randome = (min, max) => {
+  max = colorNamess.length - 1;
+  const rande = min + Math.round(Math.random() * (max - min));
+  return rande;
+};
+
+// найти элементы для изменения свойств
+let paragraphss = document.getElementsByClassName("letterss");/*если поставить вместо letters text-footer, то все буквы будет мигать одинаково*/
+
+// менять цвет каждую секунду, присвоив каждому эл-ту массива paragraphs новое значение
+setInterval(() => {
+  for (let i = 0; i < paragraphss.length; i++) {
+    paragraphss[i].style.color = colorNamess[random(0)];
+  }
+}, 3000);
+console.log(colorNamess);
